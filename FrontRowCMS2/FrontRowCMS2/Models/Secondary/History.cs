c851862198e807bf4febb4bf298add3e05a046bf
Unknown <christopherwoodhave@weber.edu>
2018-03-14 08:53:16 -0600

    Added Models and DbInitializer
    Added Models for Footer, History, Person, and User.
    
    Added code for seeding the DB including DbInitializer and updates to DBContext and Program.cs files

---
+++ FrontRowCMS2/FrontRowCMS2/Models/Secondary/History.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontRowCMS2.Models.Secondary
+{
+    public class History
+    {
+        public int ID { get; set; }
+        public string TextArea1 { get; set; }
+        public string TextArea2 { get; set; }
+        public string Image1 { get; set; }
+        public string Image2 { get; set; }
+        public string Image3 { get; set; }
+        public string Caption1 { get; set; }
+        public string Caption2 { get; set; }
+        public string Caption3 { get; set; }
+    }
+}
---
+++ FrontRowCMS2/FrontRowCMS2/Models/Secondary/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontRowCMS2.Models.Secondary
+{
+    public enum PersonType
+    {
+        Director,Staff,Both
+    }
+
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Title1 { get; set; }
+        public string Title2 { get; set; }
+        public string Email { get; set; }
+        public PersonType Type { get; set; }
+        
+    }
+}
