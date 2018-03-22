fa4dcd5385b77a6928538af3bc43aefd47c61680
Unknown <averyshort@mail.weber.edu>
2018-03-22 14:09:14 -0600

    implemented MediaEvent model
    created MediaEvent class

---
+++ FrontRowCMS2/FrontRowCMS2/Models/Secondary/MediaEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontRowCMS2.Models.Secondary
+{
+    public class MediaEvent
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Time { get; set; }
+        public string Image { get; set; }
+        public string Caption { get; set; }
+        public string Description { get; set; }
+    }
+}
